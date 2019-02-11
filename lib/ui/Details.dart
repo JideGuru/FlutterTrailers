a65dd8ae01fb78d657e3effd5af6212143e7815e
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-02-11 02:33:21 +0100

    Replaced the card with a simple list instead

--- lib/ui/Details.dart
+++ lib/ui/Details.dart
@@ -61,69 +61,124 @@ class _DetailsState extends State<Details> {
         ),
       ),
 
+
+
       body: isLoading
           ? Center(
         child: CircularProgressIndicator(
         ),
-      ) :Container(
-        child: Column(
-          children: <Widget>[
-//            Row(
-//              children: <Widget>[
-//                Image.network(
-//                  widget.img,
-//                  height: 250.0,
-//                  fit: BoxFit.cover,
-//                ),
-//              ],
-//            ),
-            Container(
-              child: Image.network(
-                widget.img,
-                height: 200.0,
-                width: 400.0,
-                fit: BoxFit.cover,
-              ),
-              height: 200.0,
-              width: 400.0,
-            ),
+      ) :Center(
+        child: ListView.separated(
+          itemCount: data == null ? 0 : data.length,
+          padding: EdgeInsets.all(16.0),
+          separatorBuilder: (BuildContext context, int position){
+            return Divider();
+          },
+          itemBuilder: (BuildContext context, int position) {
+            String img = "${Config.ytImg}${data[position]["key"]}/0.jpg";
+            String message = "Are you sure you want to download ${data[position]['name']}";
+            return ListTile(
+              title: Text("${data[position]['name']}"),
 
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(top: 5.0),
-              child: Text(
-                "${widget.header}",
-                style: TextStyle(
-                    fontSize: 20.0
-                ),
+              subtitle: Text("${data[position]['site']}"),
+
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Image.network(img),
               ),
-            ),
-            Container(
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0,bottom: 10.0),
-                itemBuilder: (BuildContext context, int position){
-                  String img = "${Config.ytImg}${data[position]["key"]}/0.jpg";
-                  return Card(
-                    elevation: 4.0,
-                    margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.all(10.0),
-                          child: Image.network(img),
-                        ),
-                      ],
-                    )
-                  );
-                },
-                itemCount: data == null ? 0 : data.length,
-              ),
-            )
-          ],
+
+
+              onTap: (){
+                _showAlertMessage(context, message);
+              },
+            );
+          },
         ),
       ),
 
 
+
+//      body: isLoading
+//          ? Center(
+//        child: CircularProgressIndicator(
+//        ),
+//      ) :Container(
+//        child: Column(
+//          children: <Widget>[
+////            Row(
+////              children: <Widget>[
+////                Image.network(
+////                  widget.img,
+////                  height: 250.0,
+////                  fit: BoxFit.cover,
+////                ),
+////              ],
+////            ),
+//            Container(
+//              child: Image.network(
+//                widget.img,
+//                height: 200.0,
+//                width: 400.0,
+//                fit: BoxFit.cover,
+//              ),
+//              height: 200.0,
+//              width: 400.0,
+//            ),
+//
+//            Container(
+//              alignment: Alignment.center,
+//              margin: EdgeInsets.only(top: 5.0),
+//              child: Text(
+//                "${widget.header}",
+//                style: TextStyle(
+//                    fontSize: 20.0
+//                ),
+//              ),
+//            ),
+//            Container(
+//              child: ListView.builder(
+//                padding: EdgeInsets.only(top: 10.0,bottom: 10.0),
+//                itemBuilder: (BuildContext context, int position){
+//                  String img = "${Config.ytImg}${data[position]["key"]}/0.jpg";
+//                  return Card(
+//                    elevation: 4.0,
+//                    margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
+//                    child: Row(
+//                      children: <Widget>[
+//                        Container(
+//                          margin: EdgeInsets.all(10.0),
+//                          child: Image.network(img),
+//                        ),
+//                      ],
+//                    )
+//                  );
+//                },
+//                itemCount: data == null ? 0 : data.length,
+//              ),
+//            )
+//          ],
+//        ),
+//      ),
+
+
     );
+  }
+
+  //Function to Show Alert Dialog for showing messages
+  void _showAlertMessage(BuildContext context, String message){
+    var alert = new AlertDialog(
+      title: Text("Download Trailer"),
+      content: Text("$message"),
+
+      actions: <Widget>[
+
+        FlatButton(
+          onPressed: (){Navigator.pop(context);},
+          child: Text("OK"),
+        )
+      ],
+    );
+
+    showDialog(context: context, builder: (context)=> alert);
   }
 }
