4279eb4ed65a3b7d360d6e1d1f72dbe8700254b4
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-02-11 01:35:56 +0100

    Added Movie Name to movie detail page

--- lib/ui/Details.dart
+++ lib/ui/Details.dart
@@ -47,12 +47,23 @@ class _DetailsState extends State<Details> {
               child: Image.network(
                 widget.img,
                 height: 200.0,
-                width: 370.0,
+                width: 400.0,
                 fit: BoxFit.cover,
               ),
               height: 200.0,
-              width: 370.0,
+              width: 400.0,
             ),
+
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 5.0),
+              child: Text(
+                "${widget.header}",
+                style: TextStyle(
+                    fontSize: 20.0
+                ),
+              ),
+            )
           ],
         ),
       ),
